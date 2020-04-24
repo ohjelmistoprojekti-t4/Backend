02c526beb57d06965d0fcce69e8e684bac4e1fbb
BuildTools <unconfigured@null.spigotmc.org>
2020-04-25 00:22:37 +0300

    Backend 1.0

---
+++ src/test/java/com/example/kyselyBack/KyselyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kyselyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
