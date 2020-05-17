b5dd8b4a95f40288bedbb43d0dd9e36480a01819
ohjelmistoprojekti-t4 <unconfigured@null.spigotmc.org>
2020-05-17 20:41:26 +0300

    Implementing spring security fixes

--- src/main/java/com/example/kyselyBack/CorsFilter.java
+++ src/main/java/com/example/kyselyBack/CorsFilter.java
@@ -13,7 +13,7 @@ public class CorsFilter extends OncePerRequestFilter {
 
         @Override
         protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-            response.setHeader("Access-Control-Allow-Origin", "*");
+        	response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
             response.setHeader("Access-Control-Allow-Credentials", "true");
             response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             response.setHeader("Access-Control-Max-Age", "3600");
